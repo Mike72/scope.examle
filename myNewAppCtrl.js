3dd3d21137f4a60896e27b3207498b62f59f3222
Mike72 <mike.zoltan72@hotmail.com>
2017-11-15 17:33:23 +0200

    $scope function(){if(){....}

--- myNewAppCtrl.js
+++ myNewAppCtrl.js
@@ -1,6 +1,13 @@
-app.controller("myNewAppCtrl",function($scope){
-
-
-
-    
-})
+app.controller("myNewAppCtrl", function($scope){
+$scope.x = 0;
+$scope.y = 0;
+$scope.sum = function(){return $scope.x + $scope.y};
+$scope.minus = function(){return $scope.x - $scope.y};
+$scope.total = function(){return $scope.x * $scope.y};
+$scope.divide = function(){
+    if($scope.y === 0){
+        return  "Can't divide by 0"
+    }else{
+    return $scope.x / $scope.y}
+}
+});
